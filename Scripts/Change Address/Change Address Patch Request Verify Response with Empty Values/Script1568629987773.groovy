8fb2e4f270b71c10418c298f26897d61d9ef12f3
Khaja Hussain <poolakhaja@gmail.com>
2019-11-27 17:27:19 +0530

    modified files

--- Scripts/Change Address/Change Address Patch Request Verify Response with Empty Values/Script1568629987773.groovy
+++ Scripts/Change Address/Change Address Patch Request Verify Response with Empty Values/Script1568629987773.groovy
@@ -35,6 +35,7 @@ for (int i = 11; i < 12; i++) {
 
     WS.verifyResponseStatusCode(response, 200)	
 	
+	
     WS.verifyElementPropertyValue(response, 'changeAddressServiceOperationResponse.RESPONSE.Output.xStatus.applicationCd', 
         findTestData('Z-Connect Test Data/Change Address_TestData').getValue(15, i))
 
