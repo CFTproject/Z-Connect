4b73a1e1227ce56be732114928560033281881fb
Khaja Hussain <poolakhaja@gmail.com>
2019-12-04 11:47:09 +0530

    files are modified

--- Test Suites/Change Restraint Duplicate Check.ts
+++ Test Suites/Change Restraint Duplicate Check.ts
@@ -10,4 +10,28 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>b36a48e6-3ea7-4b58-bbc1-2bf1f814c1c2</testSuiteGuid>
+   <testCaseLink>
+      <guid>534feced-7b98-46c0-a1d8-7f0f6d2189cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Change Restraint Duplicate Check/Change record with all blank input fields non blank account no and all other blank input fields</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3f758c18-1003-413e-8e9a-afb2513eb76a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Change Restraint Duplicate Check/Change record with all valid non blank input fields for an non-existing record</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>61fbd209-1669-47cc-8084-f7b7f0b1648a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Change Restraint Duplicate Check/Change record with non blank input fields Low check no greter than High Check No Invalid placed date blank placed date</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0ae3c9e6-f2eb-4468-92cf-6a24ab9e7ed4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Change Restraint Duplicate Check/Change record with non blank product code and account no fields and all other blank input fields less than High Check No</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
